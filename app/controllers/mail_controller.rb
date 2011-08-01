a762cca78b7342401103329bfae1557aa7cd67af
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2011-08-01 23:31:59 +0400

    logger

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -3,8 +3,8 @@ class MailController < ApplicationController
 
   def receiver
     message = Mail.new(params[:message])
-    Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
-    Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
+    Rails.logger.fatal message.subject #print the subject to the logs
+    Rails.logger.fatal message.body.decoded #print the decoded body to the logs
 
     # Do some other stuff with the mail message
 
