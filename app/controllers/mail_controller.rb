98f96c60e2fe24d50a5785c5831384163b5bc6ae
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2011-08-02 00:12:36 +0400

    mail to utf-8

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -3,8 +3,10 @@ class MailController < ApplicationController
 
   def receiver
     message = Mail.new(params[:message])
-    Rails.logger.fatal message.subject #print the subject to the logs
-    Rails.logger.fatal message.body.decoded #print the decoded body to the logs
+    to   = "UTF-8"
+    from = "iso-8859-5"
+    Rails.logger.fatal Iconv.conv(to, from, message.subject) #print the subject to the logs
+    Rails.logger.fatal Iconv.conv(to, from, message.body.decoded) #print the decoded body to the logs
 
     # Do some other stuff with the mail message
 
