4dc7ed0fadbdfe036ac9f9187de9c4bbf794ace0
kiote <krivich.ekaterina@gmail.com>
2011-08-18 19:58:16 +0400

    static css and js path

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,6 @@ module Coupon
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
     config.assets.enabled = true
+    config.action_controller.perform_caching = true
   end
 end
