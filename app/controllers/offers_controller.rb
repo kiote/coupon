c9cd8927b6344f64f21fa0113da27d6023573bcb
kiote <krivich.ekaterina@gmail.com>
2011-08-19 23:09:29 +0400

    see #3 недопоиск

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,8 @@
 class OffersController < ApplicationController
   def index
-    @offers = Offer.paginate(:page => params[:page], :per_page => 40).order("id DESC").all
+    @offers = Offer.search(params[:search])
+      .paginate(:page => params[:page], :per_page => 40)
+      .order("id DESC")
+      .all
   end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,11 @@
 class Offer < ActiveRecord::Base
   belongs_to :daily_mail
+
+  def self.search(search)
+    if search
+      where('title LIKE ?', "%#{search}%")
+    else
+      scoped
+    end
+  end
 end
